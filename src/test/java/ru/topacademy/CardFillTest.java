b6b5c0555a16fb821ce9a59c9d42820216e068cf
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-09-07 22:40:04 +0500

    tests 2city 1date

--- src/test/java/ru/topacademy/CardFillTest.java
+++ src/test/java/ru/topacademy/CardFillTest.java
@@ -2,6 +2,7 @@ package ru.topacademy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -34,5 +35,41 @@ public class CardFillTest {
         $("[data-test-id=notification] .notification__title").shouldHave(exactText("Успешно!"), Duration.ofSeconds(20));
         $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + planningDate),
                 Duration.ofSeconds(20));
+    }
+
+    @Test
+    public void testCheckCity() {
+        String planningDate = createDate(9, "dd.MM.yyyy");
+
+        $("[data-test-id=city] input").setValue("Париж");
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=name] input").setValue("Кузьма Петров-Водкин");
+        $("[data-test-id=phone] input").setValue("+79000000000");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"), Duration.ofSeconds(20));
+    }
+
+    @Test
+    public void testNotCity() {
+        String planningDate = createDate(3, "dd.MM.yyyy");
+
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=name] input").setValue("Кузьма Петров-Водкин");
+        $("[data-test-id=phone] input").setValue("+79000000000");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"), Duration.ofSeconds(20));
+    }
+
+    @Test
+    public void testNotDate() {
+        $("[data-test-id=city] input").setValue("Екатеринбург");
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=name] input").setValue("Кузьма Петров-Водкин");
+        $("[data-test-id=phone] input").setValue("+79000000000");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Неверно введена дата"), Duration.ofSeconds(20));
     }
 }
