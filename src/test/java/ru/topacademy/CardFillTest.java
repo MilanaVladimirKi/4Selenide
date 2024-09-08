3c1e6a037efd574dee837004030b5e9b39290f19
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-09-08 16:40:39 +0500

    tests 2phone 3name

--- src/test/java/ru/topacademy/CardFillTest.java
+++ src/test/java/ru/topacademy/CardFillTest.java
@@ -72,4 +72,85 @@ public class CardFillTest {
         $(".button").click();
         $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Неверно введена дата"), Duration.ofSeconds(20));
     }
+
+    @Test
+    public void testNoValName() {
+        String planningDate = createDate(11, "dd.MM.yyyy");
+
+        $("[data-test-id=city] input").setValue("Екатеринбург");
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=name] input").setValue("1731!@№# Петров-Водкин");
+        $("[data-test-id=phone] input").setValue("+79000000000");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."), Duration.ofSeconds(20));
+    }
+
+    @Test
+    public void test3Name() {
+        String planningDate = createDate(3, "dd.MM.yyyy");
+
+        $("[data-test-id=city] input").setValue("Екатеринбург");
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=name] input").setValue("Кузьма Петрович Водкин");
+        $("[data-test-id=phone] input").setValue("+79000000000");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаны неверно"), Duration.ofSeconds(20));
+    }
+
+    @Test
+    public void testNotName() {
+        String planningDate = createDate(3, "dd.MM.yyyy");
+
+        $("[data-test-id=city] input").setValue("Екатеринбург");
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=phone] input").setValue("+79000000000");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"), Duration.ofSeconds(20));
+    }
+
+    @Test
+    public void testNoValPhone() {
+        String planningDate = createDate(7, "dd.MM.yyyy");
+
+        $("[data-test-id=city] input").setValue("Екатеринбург");
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=name] input").setValue("Кузьма Петров-Водкин");
+        $("[data-test-id=phone] input").setValue("+790000000");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."), Duration.ofSeconds(20));
+    }
+
+    @Test
+    public void testNotPhone() {
+        String planningDate = createDate(7, "dd.MM.yyyy");
+
+        $("[data-test-id=city] input").setValue("Екатеринбург");
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=name] input").setValue("Кузьма Петров-Водкин");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"), Duration.ofSeconds(20));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
